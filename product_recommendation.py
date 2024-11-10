659c3a2f28c89a20151846392c1058b25b40de30
Saranyukrishna <saranyukrishna176@gmail.com>
2024-11-10 12:58:28 +0530

    Update product_recommendation.py

--- product_recommendation.py
+++ product_recommendation.py
@@ -7,24 +7,19 @@ def load_pickle_file(file_path='product.pkl'):
     with open(file_path, 'rb') as file:
         return pickle.load(file)
 
-
-# Helper function to find similar products
 def find_similar_products(product_name, df, num_results=10):
     df['similarity'] = df['product_name'].apply(lambda x: product_name.lower() in x.lower())
     similar_products = df[df['similarity']].head(num_results)
     return similar_products[['brand', 'product_name']]
 
 
-# Streamlit App
 st.title("Product Recommendation System")
 
-# Load the dataset
 try:
     df = load_pickle_file()  # Default file path to 'product.pkl'
     st.success("Data loaded successfully!")
     st.dataframe(df)  # Show first 5 rows of the data
 
-    # Search for similar products
     product_name = st.text_input("Enter Product Name to Search:", "")
     if st.button("Find Similar Products"):
         if product_name:
